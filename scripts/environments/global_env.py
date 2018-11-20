794fce8910223f67336240902566f2d83a6d88ce
YeonhwaCha <yeonhwa@phantom.ai>
2018-11-20 08:46:25 -0800

    11/20/2018 Load glbal environment module

---
+++ scripts/environments/global_env.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+CURRENT_DIR = os.getcwd()
+
+# pre-trained
+WEIGHTS_DIR  = '/weights/'
+CKPT_PATH    = os.path.join(WEIGHTS_DIR, '{}.ckpt')
+
+# dataset
+DATASET_DIR  = '/DB/imagenet'
+
